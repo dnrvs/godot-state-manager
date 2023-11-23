c9c3b17e74dfb9fd7ed809a0af2d457bbe3e60fe
dnrvs <edenmen92@outlook.com>
2023-11-23 13:52:36 -0600

    new signal system implemented in the demo

--- demo/Character.gd
+++ demo/Character.gd
@@ -6,7 +6,23 @@ const SPEED: float = 150.0
 @onready var state_manager = $StateManager
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction: int = -1
+var direction: int = 1
+var final_direction: int = direction
+
+func _enter_tree() -> void:
+	$StateManager/WalkUntilCollide.started_state.connect(func ():
+		direction *= -1
+		final_direction = direction
+		$AnimatedSprite2D.play("walk")
+	)
+	$StateManager/Attack.started_state.connect(func ():
+		final_direction = 0
+		$AnimatedSprite2D.play("attack")
+	)
+	$StateManager/Idle.started_state.connect(func ():
+		final_direction = 0
+		$AnimatedSprite2D.play("idle")
+	)
 
 func _physics_process(delta) -> void:
 	var current_state = state_manager.get_current_state_tag()
@@ -15,25 +31,12 @@ func _physics_process(delta) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	var final_direction = Vector2.ZERO
-	var anim = "idle"
-	
-	match current_state:
-		"walk_until_collide":
-			final_direction = direction
-			anim = "walk"
-		"attack":
-			anim = "attack"
-		"idle":
-			pass
-	
 	if final_direction:
 		velocity.x = final_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 	$AnimatedSprite2D.flip_h = true if direction == -1 else false
-	$AnimatedSprite2D.play(anim)
 
 func collided_with_target() -> bool:
 	var collided = false
@@ -41,7 +44,3 @@ func collided_with_target() -> bool:
 		if area.is_in_group("target"):
 			collided = true
 	return collided
-
-func _on_state_manager_state_changed(state_tag):
-	if state_tag == "walk_until_collide":
-		direction *= -1
