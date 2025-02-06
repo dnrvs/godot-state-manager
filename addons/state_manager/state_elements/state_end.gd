661d5be4ec7c2846e6878b20bbb3e70cfaf1b735
dnrvs <edenmen92@outlook.com>
2025-02-05 21:01:36 -0600

    removed custom_condition_expression_base and condition_signal for state_start and state_end

--- addons/state_manager/state_elements/state_end.gd
+++ addons/state_manager/state_elements/state_end.gd
@@ -3,4 +3,9 @@ extends State
 class_name StateEnd
 
 func _validate_property(property: Dictionary) -> void:
-	if property.name == "condition_expression": property.usage = PROPERTY_USAGE_NO_EDITOR
+	if (
+			property.name == "custom_condition_expression_base" or
+			property.name == "condition_signal" or
+			property.name == "condition_expression"
+	): 
+		property.usage = PROPERTY_USAGE_NO_EDITOR
--- addons/state_manager/state_elements/state_start.gd
+++ addons/state_manager/state_elements/state_start.gd
@@ -6,4 +6,9 @@ func _init() -> void:
 	self.condition_expression = "true"
 
 func _validate_property(property: Dictionary) -> void:
-	if property.name == "condition_expression": property.usage = PROPERTY_USAGE_NO_EDITOR
+	if (
+			property.name == "custom_condition_expression_base" or
+			property.name == "condition_signal" or
+			property.name == "condition_expression"
+	): 
+		property.usage = PROPERTY_USAGE_NO_EDITOR
